dd2d1f80fe81540efeb2fb9ff2b5aabaa976e438
Brian Preston <brian8181@gmail.com>
2022-02-03 19:04:29 -0600

    another step ...

--- cgi/find_tags.cpp
+++ cgi/find_tags.cpp
@@ -35,7 +35,7 @@ int main(int argc, char* argv[])
         try
 		{
 			//src_epx = regex("^{(.*)}$");
-            src_epx = regex("\\{\\$\\w+\\}");
+            src_epx = regex("\\{\\$[A-Za-z]+\\w*\\}");
 		}
 		catch(regex_error& e)
 		{
