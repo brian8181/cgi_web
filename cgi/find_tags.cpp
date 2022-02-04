80c77857b93be2826e839d798e5855483b6d76e0
Brian Preston <brian8181@gmail.com>
2022-02-04 10:11:17 -0600

    changes to find_tags.cpp, uses utility

--- cgi/find_tags.cpp
+++ cgi/find_tags.cpp
@@ -3,6 +3,7 @@
 #include <regex>
 #include <fstream>
 #include <string>
+#include "utility.hpp"
 
 using namespace std;
 
@@ -13,24 +14,10 @@ int main(int argc, char* argv[])
     {
         // todo: validate inputs !
         string path = argv[1];
+        string src = fstream_readlines(path);
 
-        string src;
-        fstream file;
-        file.open(path, ios::in); //open a file to perform read operation using file object
-        if (file.is_open())
-        {   
-            //checking whether the file is open
-            string tp;
-            while(getline(file, tp))
-            { 
-                //read data from file object and put it into string.
-                src += tp += "\n";
-            }
-            file.close(); //close the file object.
-        }
-
-        //cout << src << endl;
-        string name_exp = "\\$[A-Za-z]+\\w*";
+        //string name_exp = "\\$[A-Za-z]+\\w*";
+        //regex src_epx = regex("\\{\\s*\\$(" + name_exp + ")\\s*\\}");
         regex src_epx = regex("\\{\\s*\\$([A-Za-z]+\\w*)\\s*\\}");
         
         auto begin = sregex_iterator(src.begin(), src.end(), src_epx);
