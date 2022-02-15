1d7d40dc22907691690cd0f309ad1fcccf9c74ad
Brian Preston <brian8181@gmail.com>
2022-02-15 03:29:10 -0600

    find tags for comment and  config

--- cgi/find_tags.cpp
+++ cgi/find_tags.cpp
@@ -13,16 +13,21 @@ int main(int argc, char* argv[])
         // todo: validate inputs !
         string path = argv[1];
         string src = fstream_readlines(path);
-        regex src_epx = regex("\\{(.*)}", regex::ECMAScript);
+        //regex src_epx = regex(".*", regex::ECMAScript);
+        regex src_exp =  regex("\\{\\s*(((\\*)|(#))?\\s*([\\w\\$]*)\\s*((\\*)|(#))?)\\s*\\}", regex::ECMAScript);
               
-        sregex_iterator begin = sregex_iterator(src.begin(), src.end(), src_epx, std::regex_constants::match_not_null);
+        sregex_iterator begin = sregex_iterator(src.begin(), src.end(), src_exp, std::regex_constants::match_not_null);
         auto end = sregex_iterator(); 
         
 		for (sregex_iterator iter = begin; iter != end; ++iter)
 		{
             smatch match = *iter;
-            std::ssub_match sub = match[1];
-            cout << match.str() <<  " --> " << trim(sub.str()) << endl;
+            
+            bool comment = match[3].matched;
+            bool config = match[4].matched;
+
+            std::ssub_match sub = match[5];
+            cout << match.str() <<  " --> " << trim(sub.str()) << " : " << comment << " : " << config << endl;
         }
     }
 }
