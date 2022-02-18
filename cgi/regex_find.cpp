bcea0913a95ecf7415436af7ed8389e240ec9f22
Brian Preston <brian8181@gmail.com>
2022-02-18 11:24:03 -0600

    fix tabs

--- cgi/regex_find.cpp
+++ cgi/regex_find.cpp
@@ -17,12 +17,12 @@ int main(int argc, char* argv[])
         string expr_path = argv[2];
         string src = fstream_readlines(file_path);
         string expr = fstream_readlines(expr_path);
-              
+                
         regex src_exp = regex(expr, regex::ECMAScript);
         sregex_iterator begin = sregex_iterator(src.begin(), src.end(), src_exp, std::regex_constants::match_default);
         sregex_iterator end = sregex_iterator(); 
         for (sregex_iterator iter = begin; iter != end; ++iter)
-		{
+        {
             smatch match = *iter;
             //remove newline from match
             string m = match.str();
