3778d6e82e527b49b699ce9068bcaea993a0ff35
Brian Preston <brian8181@gmail.com>
2022-02-23 12:51:03 -0600

    continue find tags ...

--- cgi/find_tags.cpp
+++ cgi/find_tags.cpp
@@ -37,22 +37,24 @@ int main(int argc, char* argv[])
         // string whole_tag = start + cmd1 + name + cmd2 + ending;
       
         string cmds = "\\{\\s*(config_load|include|insert|literal|strip|capture|section|assign|debug|eval|fetch|math)\\s*\\}";
-        //string param = "\\{\\s*\\$([A-Za-z]+\\w*)\\b\\s*\\}";      // {$ABC}
-        string param = "\\{\\s*\\$([A-Za-z]+\\w*)\\s*\\}";           // {$ABC}
-        //string config =  "\\{\\s*#([^\\}]+)#\\s*\\}";              // {#ABC#}
-        string config =  "\\{\\s*#([A-Aa-z]\\w*)#\\s*\\}";           // {#ABC#}
-        //string comment = "\\{\\s*\\*[^\\}]+\\*\\s*\\}";            // {*ABC*}
-        string comment =  "\\{\\s*\\*[\\w\\s\\p]*\\*\\s*\\}";        // {*ABC*}
-        string string_literal1 = "(\\\"[\\w\\.]*\\\")";                     // "ABC"
-        string string_literal2 = "(\\'[\\w*\\.]*\\')";                       // 'ABC'
+        //string variable = "\\{\\s*\\$([A-Za-z]+\\w*)\\b\\s*\\}";            // {$ABC}
+        string variable = "\\{\\s*\\$([A-Za-z]+\\w*)\\s*\\}";                 // {$ABC}
+        //string config =  "\\{\\s*#([^\\}]+)#\\s*\\}";                    // {#ABC#}
+        string config =  "\\{\\s*#([A-Aa-z]\\w*)#\\s*\\}";                 // {#ABC#}
+        //string comment = "\\{\\s*\\*[^\\}]+\\*\\s*\\}";                  // {*ABC*}
+        string comment =  "\\{\\s*\\*[\\w\\s\\p]*\\*\\s*\\}";              // {*ABC*}
+        string string_literal1 = "(\\\"[\\w\\.]*\\\")";                    // "ABC"
+        string string_literal2 = "(\\'[\\w*\\.]*\\')";                     // 'ABC'
         string string_literal = string_literal1 + "|" + string_literal2;
 
         // cmd params
         string cmd = "(config_load|include|insert|literal|strip|capture|section|assign|debug|eval|fetch|math)";
-        string cmd_params = "\\{(" + cmds + "\\=(" + param + "|" + string_literal + "))\\}"; // {include=("ABC.cgi")} // {config_load=("ABC.conf")}
+        string cmd_variable = "\\{(" + cmds + "\\=(" + variable + "|" + string_literal + "))\\}"; // {include=("ABC.cgi")} // {config_load=("ABC.conf")}
         string file = "file=(" + string_literal + ")";
-        string array = "\\{\\s*\\$([A-Za-z]+)\\[([0-9]+)\\]\\s*\\}";
-        
+        string array = "\\{\\s*\\$([A-Za-z]+)\\[([0-9]+)\\]\\s*\\}";       // {$abc[0]}
+        string tag_with_vairable_modifier = "\\{\\$([A-Za-z]+\\w*|\\w+)\\}";               // {$ABC|abc}
+        string vairable_modifier = "\\w+(\\|\\w)*";
+                
         //regex src_exp =  regex(param, regex::ECMAScript);
         //regex src_exp =  regex(config, regex::ECMAScript);
         //regex src_exp =  regex(comment, regex::ECMAScript); 
@@ -61,7 +63,7 @@ int main(int argc, char* argv[])
         std::ostringstream exprs; 
         //exprs << cmds << "|" << array << "|" << config << "|" << comment;
         //exprs << cmds << "|" << param << "|" << config << "|" << comment;
-        exprs << cmds << "|" << param << "|" << array << "|" << config << "|" << file << "|" << string_literal << "|" << comment;
+        exprs << cmds << "|" << variable << "|" << array << "|" << config << "|" << file << "|" << string_literal << "|" << comment;
         regex src_exp = regex(exprs.str(), regex::ECMAScript);
               
         sregex_iterator begin = sregex_iterator(src.begin(), src.end(), src_exp);
