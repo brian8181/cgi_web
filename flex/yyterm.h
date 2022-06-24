56ef15945335e6a1c63b6c100a81d6f368f446ce
Brian K Preston <brian8181@gmail.com>
2022-06-24 16:29:36 -0500

    commit all

---
+++ flex/yyterm.h
@@ -0,0 +1,7 @@
+/* No semi-colon after return; correct usage is to write "yyterminate();" -
+ * we don't want an extra ';' after the "return" because that will cause
+ * some compilers to complain about unreachable statements.
+ */
+#ifndef yyterminate
+#define yyterminate() return YY_NULL
+#endif
