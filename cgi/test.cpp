9add9995275cdfa8063279815273d4483ab63512
Brian K Preston <brian8181@gmail.com>
2022-06-24 08:53:34 -0500

    commit all

---
+++ cgi/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "cgicc/CgiDefs.h"
+#include "cgicc/Cgicc.h"
+#include "cgicc/HTTPHTMLHeader.h"
+#include "cgicc/HTMLClasses.h"
+
+using namespace std;
+using namespace cgicc;
+
+int main()
+{
+    Cgicc  cgi;
+    cout << HTTPHTMLHeader(); // Set up the HTML document
+    //cout << "Content-type: text/html" << endl;
+    cout << "<html>" << endl;
+    cout << "<body>" << endl;
+    cout << "<div style=\"width: 100%; font-size: 40px; font-weight: bold; text-align: center;\">" << endl;
+    cout << "CGI - CPP Test Page" << endl;
+    cout << "</div>" << endl;
+    cout << "</body>" << endl;
+    cout << "</html>" << endl;
+}
