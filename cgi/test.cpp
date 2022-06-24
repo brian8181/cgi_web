0c6ea6f630e550536ec4820e9a4dfe7b96fefda5
Brian K Preston <brian8181@gmail.com>
2022-06-24 15:36:24 -0500

    commit all

--- cgi/test.cpp
+++ cgi/test.cpp
@@ -9,13 +9,13 @@ using namespace cgicc;
 
 int main()
 {
-    Cgicc  cgi;
-    cout << HTTPHTMLHeader(); // Set up the HTML document
-    //cout << "Content-type: text/html" << endl;
+    // Cgicc  cgi;
+    // cout << HTTPHTMLHeader(); // Set up the HTML document
+    cout << "Content-type: text/html" << endl << endl; // skip a line
     cout << "<html>" << endl;
     cout << "<body>" << endl;
     cout << "<div style=\"width: 100%; font-size: 40px; font-weight: bold; text-align: center;\">" << endl;
-    cout << "CGI - CPP Test Page" << endl;
+    cout << "CGI - CPP Test Page 1.3" << endl;
     cout << "</div>" << endl;
     cout << "</body>" << endl;
     cout << "</html>" << endl;
