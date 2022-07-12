f40e73884e31752edb28f8bf95d53af768274898
Brian K Preston <brian8181@gmail.com>
2022-07-12 08:09:49 -0500

    commit all

---
+++ cgi/bash_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Content-Type: text/html"
+echo
+echo '<html>'
+echo '<body>'
+echo '<div style="width: 100%; font-size: 40px; font-weight: bold; text-align: center;">'
+echo 'CGI Script Test Page'
+echo '</div>'
+echo '</body>'
+echo '</html>'
+
