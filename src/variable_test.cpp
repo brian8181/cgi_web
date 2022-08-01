79ed5e607ecafd2b7c1940231496fa8f54b87d01
Brian K Preston <brian8181@gmail.com>
2022-08-01 11:35:28 -0500

    commit all ...

--- src/variable_test.cpp
+++ src/variable_test.cpp
@@ -7,16 +7,22 @@ using namespace std;
 
 int main(int argc, char* argv[])
 { 
-    const string template_name = "test1.tpl";
+    const string template_name = "variable_test.tpl";
     const string project_folder = "/home/brian/src/cgi_web";
     const string conf_path = project_folder + "/test/conf_test.txt";
 
-    smarty sm(project_folder + "/www/templates", project_folder + "/www/compile", project_folder + "/www/config", project_folder + "/www/cache");
-
-    string path = project_folder + "/www/templates" + "/test1.tpl";
+    string path = project_folder + "/www/templates/" + template_name;
     string src = readlines(path);
     cout << src << endl;
 
-    //string html = sm.variable("test1.tpl");
-    //cout << html << endl;
+    smarty sm(project_folder + "/www/templates", project_folder + "/www/compile", project_folder + "/www/config", project_folder + "/www/cache");
+    sm.assign("headers", "HEADERS");
+    sm.assign("page_title", "SmartyCPP Test Page");
+    sm.assign("body", "The Body");
+    sm.assign("admin_email", "admin@something.com");
+    sm.assign("version", "0.1");
+    sm.assign("version_date", "Feb, 14 2022");
+    // call variable --
+    string html = sm.variable(src);
+    cout << html << endl;
 }
