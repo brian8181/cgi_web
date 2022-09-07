727cc5fad953b745733dd8a8a15616b39b785164
Brian K Preston <brian8181@gmail.com>
2022-09-07 13:34:42 -0500

    commit all ...

--- src/lex.cpp
+++ src/lex.cpp
@@ -1,35 +1,50 @@
 #include <iostream>
 #include <string>
-#include "cgicc/CgiDefs.h"
-#include "cgicc/Cgicc.h"
-#include "cgicc/HTTPHTMLHeader.h"
-#include "cgicc/HTMLClasses.h"
-#include "../../smarty-cpp/src/smarty.hpp"
+#include <fstream>
 #include "utility.hpp"
 
+// const unsigned int DEFAULT_TEMPLATE = 1;
+// const unsigned int PROJECT_FOLDER = 2;
+
 using namespace std;
-using namespace cgicc;
 
 int main(int argc, char* argv[])
-{ 
-    // Cgicc  cgi;
-    // cout << HTTPHTMLHeader();
+{  
+    string root_path = "./src/lex_all.conf";
+    map<string, string> pairs = get_config(root_path);
 
-    const string template_name = "default.tpl";
-    const string project_folder = "/home/brian/src/cgi_web";
-    const string conf_path = project_folder + "/test/conf_test.txt";
+    const string project_folder = pairs["project_folder"];
+    const string default_template =  pairs["default_template"];
+    const string conf_path = pairs["conf_path"];
+    
+    // check for input or use default
+    string template_name = default_template;
+    if(argc == 2)
+    {
+        template_name.clear();
+        template_name = argv[1];
+    }
+    const string path = project_folder + "/test/templates/" + template_name;
 
-    smarty sm(project_folder + "/www/templates", project_folder + "/www/compile", project_folder + "/www/config", project_folder + "/www/cache"); 
-    sm.load_config(conf_path);
-    // sm.assign("headers", "HEADERS");
-    // sm.assign("page_title", "SmartyCPP Test Page");
-    // sm.assign("body", "The Body");
-    // sm.assign("admin_email", "admin@something.com");
-    // sm.assign("version", "0.1");
-    // sm.assign("version_date", "Feb, 14 2022");
-    // sm.display("default.tpl");
+    string output;
+    // read file
+    try
+    {
+        std::ifstream ifile_strm(path);
+        std::string src((std::istreambuf_iterator<char>(ifile_strm)), std::istreambuf_iterator<char>());
+        if(src == "")
+        { 
+            cout << "Error reading file, path (" + path + ") ..." << endl;
+            return 0;
+        }
 
-    sm.lex("default.tpl");
-
-    return 0;
+        // lex   
+        output = lex(src);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Error reading file ... " << e.what() << endl;
+    }
+      
+    cout << output;
 }
