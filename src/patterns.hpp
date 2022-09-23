a791865d70d96d0b375258dc24578ee9a4c6f0c1
Brian K Preston <brian8181@gmail.com>
2022-09-22 19:32:24 -0500

    commit all ...

--- src/patterns.hpp
+++ src/patterns.hpp
@@ -13,8 +13,6 @@ const string STRING_LITERAL = "(\\'[\\w*\\.]*\\')|(\\\"[\\w\\.]*\\\")";  // 'ABC
 const string COMMAND = "(config_load|include|insert|literal|strip|capture|section|assign|debug|eval|fetch|math)";
 const string COMMENT = "\\n?\\{\\s*\\*[\\w\\s\\p]*\\*\\s*\\}\\n?";
 const string CONFIG = "\\{\\s*#([A-Aa-z]\\w*)#\\s*\\}";
-
-
 const string VARIABLE = "\\{\\s*\\$(" + SYMBOL_NAME + ")\\s*\\}";
 const string INCLUDE = "\\{\\s*\\include file\\s*=\\s*\"(.*?)\"\\s*\\}";
 const string CONFIG_LOAD = "\\{\\config_load file=\"(.*?)\"\\}";
@@ -22,23 +20,15 @@ const string LOAD_CONFIG_VALUE = "((\\w+)|('(\\w+)')|(\\\"(\\w+)\\\"))";
 const string LOAD_CONFIG_NAME = "([A-Za-z]+\\w*)";
 const string COMMENT_OR_VARIABLE = "((" + COMMENT + ")" +  "|" + "(" + VARIABLE + "))";
 const string ARRAY_KEYWORD = "array\\(" + STRING_LITERAL + "\\)";
-<<<<<<< HEAD
-
 const string LIT_VAL = INT_LITERAL + "|" + BOOL_LITERAL;
 const string TEXT = "([,+_~.$\\w\\d]+)\\n";
 //const string TEXT = "([0-9]+)\\n";
 const string IF_KEYWORD = "\\{if\\s+" + SYMBOL_NAME  + "\\s*\\}\\n";
-=======
-const string TEXT = "(([,+_~.$\\s\\w\\d])+\n)|(\\s*\\$(" + SYMBOL_NAME + ")\\s*\n)";
-//const string TEXT = "([,+_~.$\\s\\w\\d]+)\\n"
-const string IF_KEYWORD = "\\{if\\s+\\$" + SYMBOL_NAME + "\\s*\\}\\n";
->>>>>>> eca746c427c5571a657f177bdac167fc8e75d27c
 const string ELSE_KEYWORD = "\\{else\\}\\n";
 const string ELSEIF_KEYWORD = "\\{else if\\s+" + SYMBOL_NAME  + "\\s*\\}\\n";;
 const string ENDIF_KEYWORD = "\\{/if\\}\\n";
 //const string IF_SEQUENCE  = "(" + IF_KEYWORD + TEXT + ENDIF_KEYWORD + ")|(" + IF_KEYWORD + TEXT + ELSE_KEYWORD + TEXT + ENDIF_KEYWORD + ")"; 
 const string IF_SEQUENCE  = "(" + IF_KEYWORD + TEXT + ENDIF_KEYWORD + ")|(" + IF_KEYWORD + TEXT + ELSE_KEYWORD + TEXT + ENDIF_KEYWORD + ")"; 
-
 const string FOREACH_KEYWORD = "\\{foreach\\s+\\$" + SYMBOL_NAME + "\\s*\\}\\n";
 const string ENDFOREACH_KEYWORD = "\\{/foreach\\}\\n";
 const string FOREACH_SEQUENCE = FOREACH_KEYWORD + TEXT + ENDFOREACH_KEYWORD;
