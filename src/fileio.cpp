2bffcd8a7bc076a01d37d7f0209e098d2afd0682
Brian K Preston <brian8181@gmail.com>
2023-10-13 21:42:09 -0500

    fix trim functions ...

--- src/fileio.cpp
+++ src/fileio.cpp
@@ -31,51 +31,32 @@ string ifs_read_all(string path)
 }
 
 // get test configuration map
-map<string, string> get_config(string path)
-{
-    ifstream file;
-    file.open(path, ios::in);
+// map<string, string> get_config(string path)
+// {
+//     ifstream file;
+//     file.open(path, ios::in);
 
-    map<string, string> config;
-    pair<string, string> config_pair;
+//     map<string, string> config;
+//     pair<string, string> config_pair;
 
-    if (file.is_open())
-    {
-        string line;
-        while (getline(file, line))
-        {
-            size_t pos = line.find('=');
-            string name = trim(line.substr(0, pos - 1));
-            string value = trim(line.substr(pos + 1));
-            pair<string, string> p(name, value);
-            config.insert(p);
-        }
-        file.close();
-    }
-    return config;
-}
+//     if (file.is_open())
+//     {
+//         string line;
+//         while (getline(file, line))
+//         {
+//             size_t pos = line.find('=');
+            
+//             // trim(line.substr(0, pos - 1));
+//             // trim(line.substr(pos + 1));
+//             //pair<string, string> p(name, value);
+//             //config.insert(p);
+//         }
+//         file.close();
+//     }
+//     return config;
+// }
 
-// trim functions
-const std::string WHITESPACE = " \n\r\t\f\v";
-
-std::string ltrim(const std::string &s)
-{
-    size_t start = s.find_first_not_of(WHITESPACE);
-    return (start == std::string::npos) ? "" : s.substr(start);
-}
-
-std::string rtrim(const std::string &s)
-{
-    size_t end = s.find_last_not_of(WHITESPACE);
-    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
-}
-
-std::string trim(const std::string &s)
-{
-    return rtrim(ltrim(s));
-}
-
-std::string &_ltrim(std::string &s)
+void ltrim(std::string &s)
 {
     auto it = std::find_if(s.begin(), s.end(),
                            [](char c)
@@ -83,10 +64,9 @@ std::string &_ltrim(std::string &s)
                                return !std::isspace<char>(c, std::locale::classic());
                            });
     s.erase(s.begin(), it);
-    return s;
 }
 
-std::string &_rtrim(std::string &s)
+void rtrim(std::string &s)
 {
     auto it = std::find_if(s.rbegin(), s.rend(),
                            [](char c)
@@ -94,10 +74,10 @@ std::string &_rtrim(std::string &s)
                                return !std::isspace<char>(c, std::locale::classic());
                            });
     s.erase(it.base(), s.end());
-    return s;
 }
 
-std::string &_trim(std::string &s)
+void trim(std::string &s)
 {
-    return _ltrim(_rtrim(s));
+    rtrim(s);
+    ltrim(s);
 }
--- src/fileio.hpp
+++ src/fileio.hpp
@@ -11,12 +11,8 @@ string ifs_read_all(string path);
 vector<string> getlines(string path);
 map<string, string> get_config(string path);
 
-string ltrim(const string &s);
-string rtrim(const string &s);
-string trim(const string &s);
-
-string& _ltrim(const string &s);
-string& _rtrim(const string &s);
-string& _trim(const string &s);
+void ltrim(string &s);
+void rtrim(string &s);
+void trim(string &s);
 
 #endif
