f8526e9351516b161897ba163fc6f5b1e9b8aa9e
Brian K <brian8181@gmail.com>
2023-11-16 09:36:12 -0600

    fix up debug task & launch

--- src/lex.cpp
+++ src/lex.cpp
@@ -45,31 +45,6 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-// get test configuration map
-// map<string, string> get_config(string path)
-// {
-//     ifstream file;
-//     file.open(path, ios::in);
-
-//     map<string, string> config;
-//     pair<string, string> config_pair;
-
-//     if (file.is_open())
-//     {
-//         string line;
-//         while (getline(file, line))
-//         {
-//             size_t pos = line.find('=');
-//             string name = line.substr(0, pos - 1);
-//             string value = line.substr(pos + 1);
-//             pair<string, string> p(trim(name), trim(value));
-//             config.insert(p);
-//         }
-//         file.close();
-//     }
-//     return config;
-// }
-
 void  lex(string& s)
 {
     cout << "Lexing..." << endl;
